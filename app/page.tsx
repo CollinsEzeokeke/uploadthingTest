12ab901f861736396649c66ce3620a4afd4f4780
Ezeokeke Collins Chidubem <collinsmecosy@gmail.com>
2024-11-30 21:56:09 +0100

    sssssssssssssssss

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,22 @@
 "use client";
 
 import { UploadButton } from "@/utils/uploadthing";
-// import { useUploadthing}
-import { generateReactHelpers } from "@uploadthing/react";
-import { OurFileRouter } from "./api/uploadthing/core";
-
-const { useUploadThing } = generateReactHelpers<OurFileRouter>();
 
 export default function Home() {
-  const { startUpload } = useUploadThing("imageUploader")
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+      <UploadButton
+        endpoint="imageUploader"
+        onClientUploadComplete={(res) => {
+          // Do something with the response
+          console.log("Files: ", res);
+          alert("Upload Completed");
+        }}
+        onUploadError={(error: Error) => {
+          // Do something with the error.
+          alert(`ERROR! ${error.message}`);
+        }}
+      />
     </main>
   );
 }
